4a465c4dbf5b9e9c5baa539c639e9a06b8346cfc
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2024-05-31 20:13:30 -0300

    style: change the font to Inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next';
-import { Poppins } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import { Header } from '@/components/header';
 import './globals.css';
 
-const inter = Poppins({
+const inter = Inter({
   subsets: ['latin'],
   weight: '500',
 });
