ff3d77fd95832d69f83d0509d3a48e1dd066f61f
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2024-06-05 15:31:43 -0300

    feat: show toast notification when form is submitted

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Header } from '@/components/header';
+import { Toaster } from '@/components/ui/sonner';
 import './globals.css';
 
 const inter = Inter({
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={`${inter.className} antialiased dark`}>
         <Header />
         {children}
+        <Toaster position="top-center" richColors className="font-medium" />
       </body>
     </html>
   );
--- src/components/add-habit-button.tsx
+++ src/components/add-habit-button.tsx
@@ -1,11 +1,29 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import { useFormStatus } from 'react-dom';
 import { Button } from './ui/button';
 import { Loader2 } from 'lucide-react';
+import { toast } from 'sonner';
 
 export function AddHabitButton() {
   const { pending } = useFormStatus();
+  const [wasPending, setWasPending] = useState(false);
+
+  async function handleStatusChange() {
+    if (wasPending && !pending) {
+      toast.success('Hábito adicionado com sucesso!');
+      setWasPending(false);
+    }
+    if (pending) {
+      await new Promise(resolve => setInterval(resolve, 600));
+      setWasPending(true);
+    }
+  }
+
+  useEffect(() => {
+    handleStatusChange();
+  }, [pending]);
 
   return (
     <Button type="submit" disabled={pending} className="bg-primary/80">
