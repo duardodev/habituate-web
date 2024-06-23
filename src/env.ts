ccc0b94bf1edc738a0b88f08437f5f691a0ddb1c
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2024-06-22 22:33:57 -0300

    chore: NextAuth configuration

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,8 @@ import z from 'zod';
 
 const envSchema = z.object({
   NEXT_PUBLIC_API_BASE_URL: z.string().url().optional(),
+  GITHUB_ID: z.string(),
+  GITHUB_SECRET: z.string(),
 });
 
 export const env = envSchema.parse(process.env);
