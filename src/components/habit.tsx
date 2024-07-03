642b599e75ac9d64df2204889a59aae3d60af08e
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2024-07-03 20:16:59 -0300

    refactor: manage completed habits state with local state in Habit component

--- src/components/habit.tsx
+++ src/components/habit.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { toggleHabit, updateHabit } from '@/app/actions';
 import { useDateStore } from '@/store/date-store';
@@ -23,7 +23,6 @@ interface Day {
 
 async function getDaysWithCompletedHabit(id: string) {
   const token = getCookie('token');
-
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/completed-habits/${id}/days`, {
     headers: {
       Authorization: `Bearer ${token}`,
@@ -31,60 +30,86 @@ async function getDaysWithCompletedHabit(id: string) {
   });
 
   const data = await response.json();
-
   return data.map((dayWithSpecificHabitCompleted: Day) => dayWithSpecificHabitCompleted.date);
 }
 
 export function Habit({ id, title }: HabitProps) {
   const { currentDate } = useDateStore();
+  const [currentTitle, setCurrentTitle] = useState(title);
+  const [temporaryTitle, setTemporaryTitle] = useState(title);
+  const [isTitleEditing, setIsTitleEditing] = useState(false);
+  const [completedDays, setCompletedDays] = useState<string[]>([]);
+
   const currentWeekDays = Array.from({ length: 7 }, (_, i) => currentDate.startOf('week').add(i, 'day'));
   const queryClient = useQueryClient();
   const today = dayjs();
 
-  const [currentTitle, setCurrentTitle] = useState(title);
-  const [temporyTitle, setTemporyTitle] = useState(title);
-  const [isEditing, setIsEditing] = useState(false);
-
-  const { data: daysWithSpecificHabitCompleted = [] } = useQuery({
+  const { data: daysWithSpecificHabitCompleted = [], isSuccess } = useQuery({
     queryKey: ['days-with-specific-comleted-habit', id],
     queryFn: () => getDaysWithCompletedHabit(id),
   });
 
-  const { mutateAsync: handleHabitToggle } = useMutation({
+  const { mutateAsync: toggleHabitFn } = useMutation({
     mutationFn: (date: string) => toggleHabit(id, date),
     onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: ['days-with-specific-comleted-habit', id],
-      });
+      queryClient.invalidateQueries({ queryKey: ['days-with-specific-comleted-habit', id] });
     },
   });
 
   const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newTitle = e.target.value;
-    setTemporyTitle(newTitle);
+    setTemporaryTitle(newTitle);
   };
 
+  useEffect(() => {
+    if (isSuccess) {
+      setCompletedDays(daysWithSpecificHabitCompleted);
+    }
+  }, [isSuccess]);
+
   async function handleHabitTitleRename() {
-    if (temporyTitle.trim() === '') {
-      setTemporyTitle(currentTitle);
-      setIsEditing(false);
+    if (temporaryTitle.trim() === '') {
+      setTemporaryTitle(currentTitle);
+      setIsTitleEditing(false);
       return;
     }
 
-    await updateHabit(id, temporyTitle);
-    setCurrentTitle(temporyTitle);
-    setIsEditing(false);
+    await updateHabit(id, temporaryTitle);
+    setCurrentTitle(temporaryTitle);
+    setIsTitleEditing(false);
+  }
+
+  async function handleHabitToggle(date: string) {
+    setCompletedDays(prev => {
+      if (prev.includes(date)) {
+        return prev.filter(d => d !== date);
+      } else {
+        return [...prev, date];
+      }
+    });
+
+    try {
+      await toggleHabitFn(date);
+    } catch (error) {
+      setCompletedDays(prev => {
+        if (prev.includes(date)) {
+          return prev.filter(d => d !== date);
+        } else {
+          return [...prev, date];
+        }
+      });
+    }
   }
 
   return (
     <li className="flex items-center justify-between">
       <div className="group flex items-center gap-6">
         <DropdownMenu>
-          <DropdownMenuTrigger disabled={isEditing}>
-            {isEditing ? (
+          <DropdownMenuTrigger disabled={isTitleEditing}>
+            {isTitleEditing ? (
               <Input
                 type="text"
-                value={temporyTitle}
+                value={temporaryTitle}
                 className="h-7 py-3 text-base focus-visible:ring-transparent"
                 onChange={handleTitleChange}
                 onBlur={handleHabitTitleRename}
@@ -98,15 +123,13 @@ export function Habit({ id, title }: HabitProps) {
               <h2 className="hover:text-foreground/85 transition-colors">{currentTitle}</h2>
             )}
           </DropdownMenuTrigger>
-          <UserActionsMenu habitId={id} onRename={() => setIsEditing(true)} />
+          <UserActionsMenu habitId={id} onRename={() => setIsTitleEditing(true)} />
         </DropdownMenu>
       </div>
-
       <div className="flex items-center gap-x-6">
         {currentWeekDays.map(currentWeekDay => {
           const isDisabled = currentWeekDay.isAfter(today, 'day');
-          const isChecked = daysWithSpecificHabitCompleted.includes(currentWeekDay.toISOString());
-
+          const isChecked = completedDays.includes(currentWeekDay.toISOString());
           return (
             <Checkbox
               key={currentWeekDay.toString()}
