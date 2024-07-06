71b0b44bea75c7eacd2a328211ec1c059c23459d
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2024-07-06 15:40:00 -0300

    style: remove maximum width from footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
   const currentYear = dayjs().year();
 
   return (
-    <footer className="max-w-[800px] h-12 px-6 pb-12 pt-20 mx-auto mt-auto">
+    <footer className="h-12 px-6 pb-12 pt-20 mx-auto mt-auto">
       <p className="text-sm text-foreground/70">
         {currentYear} © Habituate. Feito por{' '}
         <Link href="https://github.com/duardodev" target="_blank" className="underline">
