f5df6d5086b809d93a51e2f3e4eda51533204a69
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2024-07-11 18:06:41 -0300

    feat: generate a robots file to improve SEO

---
+++ src/app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: [],
+    },
+    sitemap: 'https://habituate-duardodev.vercel.app/sitemap.xml',
+  };
+}
