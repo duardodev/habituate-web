e7eeb1089aa6a36bd3fcd492a4e310a1b5f7285d
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2024-08-05 20:21:15 -0300

    fix: ensure that button on error page reloads the page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,12 +2,12 @@
 
 import { Button } from '@/components/ui/button';
 
-export default function Error({ reset }: { error: Error & { digest?: string }; reset: () => void }) {
+export default function Error({}: { error: Error & { digest?: string }; reset: () => void }) {
   return (
     <div>
       <div className="h-screen flex flex-col items-center justify-center gap-3">
         <h2>Algo deu errado!</h2>
-        <Button variant="outline" onClick={() => reset()}>
+        <Button variant="outline" onClick={() => window.location.reload()}>
           Tente novamente
         </Button>
       </div>
