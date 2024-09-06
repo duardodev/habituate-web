d45aff2b664598f798d6c443c75128a3ea66ea09
Deivit Eduardo <deiviteduardo87@gmail.com>
2024-09-06 18:11:44 -0300

    fix: ensure the checked checkbox visual appears in dark mode

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -11,7 +11,7 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      'bg-zinc-300/80 hover:bg-zinc-300/90 dark:bg-zinc-700/40 dark:hover:bg-zinc-700/60 border border-foreground/5 peer h-7 w-7 shrink-0 rounded-lg ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:bg-zinc-300/25 dark:disabled:bg-zinc-700/5 data-[state=checked]:bg-primary transition-colors',
+      'bg-zinc-300/80 hover:bg-zinc-300/90 dark:bg-zinc-700/40 dark:hover:bg-zinc-700/60 border border-foreground/5 peer h-7 w-7 shrink-0 rounded-lg ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:bg-zinc-300/25 dark:disabled:bg-zinc-700/5 data-[state=checked]:bg-primary dark:data-[state=checked]:bg-primary transition-colors',
       className
     )}
     checked={checked}
