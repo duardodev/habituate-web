d60c9bf4b5a109549085e05a10f5f28fa222a7b3
Deivit Eduardo <deiviteduardo87@gmail.com>
2024-09-08 19:46:24 -0300

    chore: ensure correct jest config and update related dependencies

--- jest.config.ts
+++ jest.config.ts
@@ -11,8 +11,7 @@ const config = {
   // Add more setup options before each test is run
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  preset: 'ts-jest',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
