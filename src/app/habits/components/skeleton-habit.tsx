740671ceacdbea874bc7982f005758340b09de12
Deivit Eduardo <deiviteduardo87@gmail.com>
2024-09-09 22:59:01 -0300

    feat(Habits): use suspense to show a skeleton when the fetch is happening

---
+++ src/app/habits/components/skeleton-habit.tsx
@@ -0,0 +1,16 @@
+import { Skeleton } from '@/components/ui/skeleton';
+
+export function SkeletonHabit() {
+  const checkboxs = Array.from({ length: 7 });
+
+  return (
+    <div className="mt-4 flex items-center justify-between">
+      <Skeleton className="h-5 w-44 rounded-xl" />
+      <div className="flex space-x-6">
+        {checkboxs.map((_, i) => {
+          return <Skeleton key={i} className="h-7 w-7" />;
+        })}
+      </div>
+    </div>
+  );
+}
--- src/app/habits/page.tsx
+++ src/app/habits/page.tsx
@@ -1,11 +1,15 @@
+import { Suspense } from 'react';
 import { Calendar } from '@/app/habits/components/calendar';
 import { Habits } from './components/habits';
+import { SkeletonHabit } from './components/skeleton-habit';
 
 export default function HabitsPage() {
   return (
     <div className="flex flex-col overflow-x-auto min-[530px]:overflow-visible">
       <Calendar />
-      <Habits />
+        <Suspense fallback={<SkeletonHabit />}>
+          <Habits />
+        </Suspense>
     </div>
   );
 }
