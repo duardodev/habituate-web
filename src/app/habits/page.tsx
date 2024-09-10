5f3dd8b014e0b52cb77289dc1d4240fbb06e3146
Deivit Eduardo <deiviteduardo87@gmail.com>
2024-09-09 23:00:42 -0300

    refactor(Habits): use error boundary to extract error logic

--- src/app/habits/page.tsx
+++ src/app/habits/page.tsx
@@ -1,15 +1,20 @@
 import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 import { Calendar } from '@/app/habits/components/calendar';
 import { Habits } from './components/habits';
 import { SkeletonHabit } from './components/skeleton-habit';
+import { HabitsErrorFallback } from './components/habits-error-fallback';
 
 export default function HabitsPage() {
   return (
     <div className="flex flex-col overflow-x-auto min-[530px]:overflow-visible">
       <Calendar />
+
+      <ErrorBoundary fallback={<HabitsErrorFallback />}>
         <Suspense fallback={<SkeletonHabit />}>
           <Habits />
         </Suspense>
+      </ErrorBoundary>
     </div>
   );
 }
