fb336c25e1c2995c5882c10d06805a2cbc18890c
Deivit Eduardo <deiviteduardo87@gmail.com>
2024-10-01 21:00:42 -0300

    chore: add msw setup

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from '@/components/theme-provider';
+import { MSWProvider } from '@/components/msw-provider';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(
@@ -19,7 +20,9 @@ export function Providers({ children }: { children: React.ReactNode }) {
 
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <MSWProvider>{children}</MSWProvider>
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
---
+++ src/components/msw-provider.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { Suspense, use } from 'react';
+
+const mockingEnabledPromise =
+  typeof window !== 'undefined' && process.env.NODE_ENV === 'development'
+    ? import('../app/api/mocks/browser').then(async ({ worker }) => {
+        await worker.start({
+          onUnhandledRequest: 'bypass',
+
+          quiet: false,
+          waitUntilReady: true,
+        });
+      })
+    : Promise.resolve();
+
+export function MSWProvider({ children }: Readonly<{ children: React.ReactNode }>) {
+  // If MSW is enabled, we need to wait for the worker to start,
+  // so we wrap the children in a Suspense boundary until it's ready.
+  return (
+    <Suspense fallback={null}>
+      <MSWProviderWrapper>{children}</MSWProviderWrapper>
+    </Suspense>
+  );
+}
+
+function MSWProviderWrapper({ children }: Readonly<{ children: React.ReactNode }>) {
+  use(mockingEnabledPromise);
+  return children;
+}
