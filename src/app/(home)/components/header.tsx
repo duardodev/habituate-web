06e60ab3888184f457f5183c5e5b33e6a9f91846
Deivit Eduardo <deiviteduardo87@gmail.com>
2024-11-11 11:59:26 -0300

    feat: add UserProfileInfo component to homepage header

--- src/app/(home)/components/header.tsx
+++ src/app/(home)/components/header.tsx
@@ -1,32 +1,28 @@
-'use client';
-
 import Link from 'next/link';
 import dynamic from 'next/dynamic';
-import { motion } from 'framer-motion';
 import { ThemeToggle } from '@/components/theme-toggle';
+import { UserProfileInfo } from '@/components/user-profile-info';
+import { currentUser } from '@clerk/nextjs/server';
 
 const Logo = dynamic(() => import('../../../components/logo').then(mod => mod.Logo), {
   ssr: false,
 });
 
-export function Header() {
+export async function Header() {
+  const user = await currentUser();
+
   return (
     <header className="max-w-[1024px] w-full px-4 pt-6 mx-auto">
-      <motion.div
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
-        transition={{ duration: 0.5, ease: 'easeInOut' }}
-        viewport={{
-          once: true,
-        }}
-        className="flex items-center justify-between"
-      >
+      <div className="flex items-center justify-between">
         <Link href="/">
           <Logo />
         </Link>
 
-        <ThemeToggle />
-      </motion.div>
+        <div className="flex items-center gap-3">
+          {user && <UserProfileInfo />}
+          <ThemeToggle />
+        </div>
+      </div>
     </header>
   );
 }
