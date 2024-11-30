157b15019e817000188b76824750776ab62c3aa9
Deivit Eduardo <deiviteduardo87@gmail.com>
2024-11-30 20:08:49 -0300

    refactor: change the env in which the mocks will start

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,7 +1,7 @@
 export async function register() {
   console.log('Running the instrumentation hook');
 
-  if (process.env.NODE_ENV === 'development' && process.env.NEXT_RUNTIME === 'nodejs') {
+  if (process.env.NODE_ENV === 'test' && process.env.NEXT_RUNTIME === 'nodejs') {
     console.log('Running in development mode, setting up MSW server');
 
     const { server } = await import('./app/api/mocks/server');
