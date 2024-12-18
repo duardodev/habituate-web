36fc55a6656bcfa01eeeddeb34616fb2fa3db9bd
Deivit Eduardo <deiviteduardo87@gmail.com>
2024-12-18 12:50:45 -0300

    feat: add tooltip to task title

--- src/app/habits/components/task-title.tsx
+++ src/app/habits/components/task-title.tsx
@@ -5,6 +5,7 @@ import { useTaskContext } from '@/hooks/use-task-context';
 import { useTaskTitleStore } from '@/store/use-task-title-store';
 import { useTaskTitle } from '@/hooks/use-task-title';
 import { cn } from '@/lib/utils';
+import { Tooltip } from '@/components/ui/tooltip';
 
 export function TaskTitle() {
   const { title, completed } = useTaskContext();
@@ -15,14 +16,16 @@ export function TaskTitle() {
       {isTitleEditing ? (
         <TitleEditor title={title} onTitleSave={handleTaskTitleUpdate} />
       ) : (
-        <p
-          className={cn(
-            'max-w-40 truncate text-sm',
-            completed ? 'text-zinc-400 dark:text-zinc-500 line-through' : 'text-zinc-900 dark:text-zinc-100'
-          )}
-        >
-          {title}
-        </p>
+        <Tooltip text={title}>
+          <p
+            className={cn(
+              'max-w-40 truncate text-sm',
+              completed ? 'text-zinc-400 dark:text-zinc-500 line-through' : 'text-zinc-900 dark:text-zinc-100'
+            )}
+          >
+            {title}
+          </p>
+        </Tooltip>
       )}
     </>
   );
