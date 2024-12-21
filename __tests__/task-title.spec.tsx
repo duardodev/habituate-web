eabcd2c36d91e0a750edf1d5b609181692f68037
Deivit Eduardo <deiviteduardo87@gmail.com>
2024-12-21 19:42:40 -0300

    test: add tests to TaskTitle component

---
+++ __tests__/task-title.spec.tsx
@@ -0,0 +1,76 @@
+import { TaskTitle } from '@/app/habits/components/task-title';
+import { useTaskContext } from '@/hooks/use-task-context';
+import { useTaskTitle } from '@/hooks/use-task-title';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+jest.mock('@/hooks/use-task-context', () => ({
+  useTaskContext: jest.fn(),
+}));
+
+jest.mock('@/hooks/use-task-title', () => ({
+  useTaskTitle: jest.fn(),
+}));
+
+describe('TaskTitle component', () => {
+  const handleTaskTitleUpdate = jest.fn();
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    (useTaskContext as jest.Mock).mockReturnValue({
+      title: 'Task 1',
+      completed: false,
+    });
+
+    (useTaskTitle as jest.Mock).mockReturnValue({
+      isTitleEditing: false,
+      handleTaskTitleUpdate,
+    });
+  });
+
+  it('should render title when not editing', () => {
+    render(<TaskTitle />);
+    expect(screen.getByRole('paragraph')).toHaveTextContent('Task 1');
+  });
+
+  it('should render TitleEditor when is editing', () => {
+    (useTaskTitle as jest.Mock).mockReturnValue({
+      isTitleEditing: true,
+    });
+
+    render(<TaskTitle />);
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
+    expect(screen.getByDisplayValue('Task 1')).toBeInTheDocument();
+  });
+
+  it('should handle title update', async () => {
+    (useTaskTitle as jest.Mock).mockReturnValue({
+      isTitleEditing: true,
+      handleTaskTitleUpdate,
+    });
+
+    render(<TaskTitle />);
+
+    const titleInput = screen.getByRole('textbox');
+    await userEvent.clear(titleInput);
+    await userEvent.type(titleInput, 'New task');
+    await userEvent.keyboard('{Enter}');
+
+    expect(handleTaskTitleUpdate).toHaveBeenCalledWith('New task');
+  });
+
+  it('should have correct CSS classes for uncompleted task', () => {
+    render(<TaskTitle />);
+    expect(screen.getByRole('paragraph')).toHaveClass('text-zinc-900 dark:text-zinc-100');
+  });
+
+  it('should have correct CSS classes for completed task', () => {
+    (useTaskContext as jest.Mock).mockReturnValue({
+      completed: true,
+    });
+
+    render(<TaskTitle />);
+    expect(screen.getByRole('paragraph')).toHaveClass('text-zinc-400 dark:text-zinc-500 line-through');
+  });
+});
---
+++ __tests__/use-task-title.spec.ts
@@ -0,0 +1,70 @@
+import { useTaskContext } from '@/hooks/use-task-context';
+import { useTaskTitle } from '@/hooks/use-task-title';
+import { useTaskTitleStore } from '@/store/use-task-title-store';
+import { useTasksStore } from '@/store/use-tasks-store';
+import { act, renderHook } from '@testing-library/react';
+
+jest.mock('@/store/use-task-title-store');
+jest.mock('@/hooks/use-task-context');
+jest.mock('@/store/use-tasks-store');
+
+describe('useTaskTitle hook', () => {
+  const mockTaskId = 'task-1';
+  const mockToggleEditingTask = jest.fn();
+  const mockUpdateTaskTitle = jest.fn();
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    (useTaskContext as jest.Mock).mockReturnValue({ id: mockTaskId });
+
+    (useTaskTitleStore as unknown as jest.Mock).mockImplementation(selector => {
+      const state = {
+        toggleEditingTask: mockToggleEditingTask,
+        activeEditingTaskId: null,
+      };
+
+      return selector(state);
+    });
+
+    (useTasksStore as unknown as jest.Mock).mockImplementation(selector => {
+      const state = {
+        updateTaskTitle: mockUpdateTaskTitle,
+      };
+
+      return selector(state);
+    });
+  });
+
+  it('should return correct initial values', () => {
+    const { result } = renderHook(() => useTaskTitle());
+    expect(result.current.isTitleEditing).toBe(false);
+    expect(typeof result.current.handleTaskTitleUpdate).toBe('function');
+  });
+
+  it('should update isTitleEditing when activeEditingTaskId matches', () => {
+    (useTaskTitleStore as unknown as jest.Mock).mockImplementation(selector => {
+      const state = {
+        toggleEditingTask: mockToggleEditingTask,
+        activeEditingTaskId: mockTaskId,
+      };
+
+      return selector(state);
+    });
+
+    const { result } = renderHook(() => useTaskTitle());
+
+    expect(result.current.isTitleEditing).toBe(true);
+  });
+
+  it('should call updateTaskTitle and toggleEditingTask when handleTaskTitleUpdate is called', () => {
+    const { result } = renderHook(() => useTaskTitle());
+
+    act(() => {
+      result.current.handleTaskTitleUpdate('New task title');
+    });
+
+    expect(mockUpdateTaskTitle).toHaveBeenCalledWith(mockTaskId, 'New task title');
+    expect(mockToggleEditingTask).toHaveBeenCalledWith(mockTaskId);
+  });
+});
