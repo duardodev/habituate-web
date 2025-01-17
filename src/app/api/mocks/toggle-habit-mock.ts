456c46cbaaa4f98429b68275deba1f55ebb97a30
Deivit Eduardo <deiviteduardo87@gmail.com>
2025-01-17 16:25:06 -0300

    refactor(toggle-habit-mock): return status code 200 as response

--- src/app/api/mocks/toggle-habit-mock.ts
+++ src/app/api/mocks/toggle-habit-mock.ts
@@ -2,6 +2,6 @@ import { http, HttpResponse } from 'msw';
 
 export const toggleHabitMock = http.patch('http://localhost:3333/habits/:id/toggle', async () => {
   return HttpResponse.json(null, {
-    status: 204,
+    status: 200,
   });
 });
