c67be3e326195da7e1278dedc69f93c9150a6c73
Deivit Eduardo <deiviteduardo87@gmail.com>
2025-01-26 19:41:09 -0300

    refactor(month-year-display): improve ux

--- src/app/management/components/month-year-display.tsx
+++ src/app/management/components/month-year-display.tsx
@@ -2,15 +2,19 @@
 
 import { monthsNames } from '@/lib/data';
 import { useDateStore } from '@/store/date-store';
+import { CalendarDays } from 'lucide-react';
 
 export function MonthYearDisplay() {
   const { currentDate } = useDateStore();
-  const currentMonth = monthsNames[currentDate.month()].slice(0, 3);
+  const currentMonth = monthsNames[currentDate.month()];
   const currentYear = currentDate.year();
 
   return (
-    <h2 className="text-foreground/95 font-medium text-nowrap">
-      {currentMonth} {currentYear}
-    </h2>
+    <div className="flex items-center gap-x-2">
+      <CalendarDays className="text-foreground/95 h-5 w-5" />
+      <h2 className="text-foreground/95 font-medium text-nowrap">
+        {currentMonth} de {currentYear}
+      </h2>
+    </div>
   );
 }
