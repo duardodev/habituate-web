5b5e373b4632ce19edaccfb3c63845f638eae021
Deivit Eduardo <deiviteduardo87@gmail.com>
2025-01-29 15:35:42 -0300

    fix: disable SSR for EmojiPickerButton to avoid client-server mismatch

--- src/app/management/components/habit.tsx
+++ src/app/management/components/habit.tsx
@@ -1,7 +1,13 @@
+import dynamic from 'next/dynamic';
 import { Checkboxes } from './checkboxes';
-import { EmojiPickerButton } from './emoji-picker-button';
 import { HabitTitle } from './habit-title';
 import { useHabit } from '@/hooks/use-habit';
+import { Skeleton } from '@/components/ui/skeleton';
+
+const EmojiPickerButton = dynamic(() => import('./emoji-picker-button').then(mod => mod.EmojiPickerButton), {
+  loading: () => <Skeleton className="h-[26px] w-[26px]" />,
+  ssr: false,
+});
 
 interface HabitProps {
   id: string;
