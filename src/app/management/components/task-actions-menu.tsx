0004de087c920440a34ad1c7ca25dbc4d9c96bae
Deivit Eduardo <deiviteduardo87@gmail.com>
2025-02-01 22:47:48 -0300

    feat(task-actions-menu): render a button to disable editing

--- src/app/management/components/task-actions-menu.tsx
+++ src/app/management/components/task-actions-menu.tsx
@@ -1,16 +1,26 @@
 'use client';
 
 import { DropdownMenu, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
-import { MoreHorizontal } from 'lucide-react';
+import { MoreHorizontal, X } from 'lucide-react';
 import { UserActionsMenu } from './user-actions-menu';
 import { useTaskContext } from '@/hooks/use-task-context';
 import { useTasksStore } from '@/store/tasks-store';
 import { useActiveTaskEditingContext } from '@/hooks/use-active-task-editing-context';
+import { useTaskTitle } from '@/hooks/use-task-title';
 
 export function TaskActionsMenu() {
   const { id } = useTaskContext();
+  const { isTitleEditing } = useTaskTitle();
   const { handleToggleTaskEditing } = useActiveTaskEditingContext();
   const removeTask = useTasksStore(state => state.removeTask);
+
+  if (isTitleEditing) {
+    return (
+      <button type="button" onClick={() => handleToggleTaskEditing(id)}>
+        <X className="w-5 h-5 text-zinc-400" />
+      </button>
+    );
+  }
 
   return (
     <DropdownMenu>
