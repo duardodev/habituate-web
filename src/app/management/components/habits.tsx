db4cd5d69b38ccdc2235568b96c69c94a888cd6d
Deivit Eduardo <deiviteduardo87@gmail.com>
2025-02-04 12:39:31 -0300

    refactor(habits): handle errors directly

--- src/app/management/components/habits.tsx
+++ src/app/management/components/habits.tsx
@@ -1,4 +1,5 @@
 import { Habit } from './habit';
+import { ErrorFallback } from './error-fallback';
 import { HabitProvider } from '@/contexts/habit-context';
 import { useFetchHabits } from '@/hooks/use-fetch-habits';
 import { auth } from '@clerk/nextjs/server';
@@ -7,35 +8,43 @@ export async function Habits() {
   const { getToken } = auth();
   const token = await getToken();
 
-  const { habits } = await useFetchHabits(token);
+  try {
+    const { habits } = await useFetchHabits(token);
 
-  if (habits.length === 0) {
+    if (habits.length === 0) {
+      return (
+        <div className="mt-4 flex items-center justify-center">
+          <p className="text-sm text-center">
+            Nenhum hábito encontrado. <br className="sm:hidden" />
+            Que tal adicionar um novo hábito para começar?
+          </p>
+        </div>
+      );
+    }
+
     return (
-      <div className="mt-4 flex items-center justify-center">
-        <p className="text-sm text-center">
-          Nenhum hábito encontrado. <br className="sm:hidden" />
-          Que tal adicionar um novo hábito para começar?
-        </p>
+      <div className="mt-4 space-y-4">
+        {habits.map(habit => {
+          return (
+            <HabitProvider
+              key={habit.id}
+              habit={{
+                id: habit.id,
+                title: habit.title,
+                emoji: habit.emoji,
+              }}
+            >
+              <Habit key={habit.id} id={habit.id} />
+            </HabitProvider>
+          );
+        })}
       </div>
     );
-  }
+  } catch (error) {
+    console.error(error);
 
-  return (
-    <div className="mt-4 space-y-4">
-      {habits.map(habit => {
-        return (
-          <HabitProvider
-            key={habit.id}
-            habit={{
-              id: habit.id,
-              title: habit.title,
-              emoji: habit.emoji,
-            }}
-          >
-            <Habit key={habit.id} id={habit.id} />
-          </HabitProvider>
-        );
-      })}
-    </div>
-  );
+    return (
+      <ErrorFallback message="Falha ao carregar os dados dos hábitos. Espere um instante e tente novamente recarregando a página." />
+    );
+  }
 }
--- src/app/management/page.tsx
+++ src/app/management/page.tsx
@@ -1,17 +1,15 @@
 import { Suspense } from 'react';
-import { ErrorBoundary } from 'react-error-boundary';
 import { Calendar } from './components/calendar';
 import { Habits } from './components/habits';
 import { HabitSkeleton } from './components/habit-skeleton';
 import { Tasks } from './components/tasks';
-import { ErrorFallback } from './components/error-fallback';
 import { Separator } from '@/components/ui/separator';
 import { DailyProgress } from './components/daily-progress';
 
 export default function ManagementPage() {
   return (
     <div className="flex flex-col overflow-visible min-h-screen">
-      <div className="mt-6 w-full max-w-[828px] mx-auto">
+      <div className="mt-9 w-full max-w-[828px] mx-auto">
         <div className="overflow-x-hidden rounded-2xl border border-border bg-white shadow-lg dark:bg-background">
           <div className="overflow-x-auto p-4">
             <div className="min-w-max">
@@ -19,15 +17,9 @@ export default function ManagementPage() {
 
               <Separator className="mt-4" />
 
-              <ErrorBoundary
-                fallback={
-                  <ErrorFallback message="Não foi possível carregar os dados dos hábitos. Espere um instante e tente novamente recarregando a página." />
-                }
-              >
-                <Suspense fallback={<HabitSkeleton />}>
-                  <Habits />
-                </Suspense>
-              </ErrorBoundary>
+              <Suspense fallback={<HabitSkeleton />}>
+                <Habits />
+              </Suspense>
             </div>
           </div>
         </div>
