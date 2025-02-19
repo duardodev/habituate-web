13099ad2953c40134394964aa0af3400f0f88602
Deivit Eduardo <deiviteduardo87@gmail.com>
2025-02-19 13:52:44 -0300

    refactor: adjust home page height and margin

--- src/app/(public)/(home)/layout.tsx
+++ src/app/(public)/(home)/layout.tsx
@@ -5,7 +5,7 @@ export default function HomeLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <Header />
-      <main className="mt-52 mb-28 h-screen flex flex-1 items-center justify-center px-4 mx-auto">
+      <main className="mt-32 h-full flex flex-1 items-center justify-center px-4 mx-auto">
         <div className="w-60 h-60 -ml-[120px] fixed top-1/2 left-1/2 bg-primary opacity-35 blur-[80px] -z-10" />
         {children}
       </main>
