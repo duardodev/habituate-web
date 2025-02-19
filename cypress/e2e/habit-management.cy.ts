8fdddddc5f5f4d7b5bf28385f31465676e4abcdf
Deivit Eduardo <deiviteduardo87@gmail.com>
2025-02-18 22:33:12 -0300

    test(habit-management): add e2e test for emoji selection

--- cypress/e2e/habit-management.cy.ts
+++ cypress/e2e/habit-management.cy.ts
@@ -28,12 +28,13 @@ describe('Habit Management', () => {
   describe('Creating New Habit', () => {
     it('should successfully create a new habit', () => {
       cy.get('button').contains('Add new habit').click();
-      cy.get('input[placeholder="Meditate, exercise, read a book..."]').type('Correr 1KM');
+      cy.get('input[placeholder="Meditate, exercise, read a book..."]').type('Correr 10KM');
       cy.get('button').contains('Confirm').click();
       cy.get('button').contains('Cancel').click();
 
-      cy.contains('Habit added successfully!').should('be.visible');
-      cy.contains('Correr 1KM').should('be.visible');
+      cy.contains('Habit successfully added!').should('be.visible');
+      cy.wait(500);
+      cy.contains('Correr 10KM').should('be.visible');
     });
 
     it('should display an error when trying to add a habit with an empty title', () => {
@@ -47,7 +48,7 @@ describe('Habit Management', () => {
 
   describe('Toggling Habit Status', () => {
     it('should set the habit status to completed', () => {
-      cy.contains('Correr 1KM').should('be.visible');
+      cy.contains('Correr 10KM').should('be.visible');
 
       cy.get('button[role="checkbox"]').filter(':enabled').last().as('checkbox');
       cy.get('@checkbox').should('be.visible');
@@ -64,7 +65,7 @@ describe('Habit Management', () => {
     });
 
     it('should set the habit status to not completed', () => {
-      cy.contains('Correr 1KM').should('be.visible');
+      cy.contains('Correr 10KM').should('be.visible');
 
       cy.get('button[role="checkbox"]').filter(':enabled').last().as('checkbox');
       cy.get('@checkbox').should('be.visible');
@@ -80,26 +81,61 @@ describe('Habit Management', () => {
     });
   });
 
+  describe('Updating Habit Emoji', () => {
+    it('should update the habit emoji and persist after reload', () => {
+      cy.contains('Correr 10KM').should('be.visible');
+
+      cy.get('button[aria-label="Open emoji picker"]').as('emojiButton');
+      cy.get('@emojiButton').click();
+
+      cy.get('.epr-main').as('emojiPicker');
+      cy.get('@emojiPicker').should('be.visible');
+
+      cy.get('[data-unified="1f600"] > .epr-emoji-img').as('selectedEmoji');
+      cy.get('@selectedEmoji')
+        .invoke('attr', 'src')
+        .then(selectedEmojiUrl => {
+          if (!selectedEmojiUrl) throw new Error('Emoji URL não encontrada');
+
+          cy.get('[data-unified="1f600"]').click();
+          cy.wait(500);
+
+          cy.get('@emojiButton')
+            .find('img')
+            .should('have.attr', 'src')
+            .and('include', encodeURIComponent(selectedEmojiUrl));
+
+          cy.reload();
+          cy.wait(1000);
+
+          cy.get('@emojiButton')
+            .find('img')
+            .should('have.attr', 'src')
+            .and('include', encodeURIComponent(selectedEmojiUrl));
+        });
+    });
+  });
+
   describe('Managing Habit Actions', () => {
     it('should edit the habit title', () => {
-      cy.contains('Correr 1KM').should('be.visible').click();
+      cy.contains('Correr 10KM').should('be.visible').click();
       cy.wait(500);
 
       cy.get('[role="menu"]').should('be.visible');
       cy.get('[role="menu"]').find('[role="menuitem"]').contains('Rename').should('be.visible').click();
 
-      cy.get('input[type="text"]').should('be.visible').clear().type('Correr 5KM').type('{enter}');
-      cy.wait(500);
-      cy.contains('Correr 5KM').should('be.visible');
+      cy.get('input[type="text"]').should('be.visible').clear().type('Correr 15KM').type('{enter}');
+      cy.wait(1500);
+      cy.contains('Correr 15KM').should('be.visible');
     });
 
     it('should remove the habit', () => {
-      cy.contains('Correr 5KM').should('be.visible').click();
+      cy.contains('Correr 15KM').should('be.visible').click();
       cy.wait(500);
 
       cy.get('[role="menu"]').should('be.visible');
       cy.get('[role="menu"]').find('[role="menuitem"]').contains('Remove').should('be.visible').click();
-      cy.contains('Correr 5KM').should('not.exist');
+      cy.contains('Correr 15KM').should('not.exist');
     });
   });
 });
