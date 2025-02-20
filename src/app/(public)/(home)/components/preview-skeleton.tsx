6d019ea4255db789a3a181f70a0fe4f34d1c479c
Deivit Eduardo <deiviteduardo87@gmail.com>
2025-02-20 16:37:07 -0300

    refactor(preview): adjust width and height

--- src/app/(public)/(home)/components/preview-skeleton.tsx
+++ src/app/(public)/(home)/components/preview-skeleton.tsx
@@ -1,5 +1,9 @@
 import { Skeleton } from '@/components/ui/skeleton';
 
 export function PreviewSkeleton() {
-  return <Skeleton className="w-[896px] h-[558.16px] bg-secondary rounded-2xl" />;
+  return (
+    <div className="max-w-[650px] w-full flex justify-center">
+      <Skeleton className="w-full max-w-[650px] aspect-[650/402] bg-secondary rounded-2xl" />
+    </div>
+  );
 }
--- src/app/(public)/(home)/components/preview.tsx
+++ src/app/(public)/(home)/components/preview.tsx
@@ -9,24 +9,24 @@ export function Preview() {
   const { resolvedTheme } = useTheme();
 
   return (
-    <div className="max-w-4xl">
+    <div className="max-w-[650px]">
       <Image
         src={previewLight}
         quality={100}
-        width={896}
-        height={558.16}
+        width={650}
+        height={402}
         alt="Light theme preview"
-        className={`border border-border shadow-2xl rounded-2xl transition-opacity duration-300 ${
+        className={`max-h-[402px] border border-border shadow-2xl rounded-2xl transition-opacity duration-300 ${
           resolvedTheme === 'dark' ? 'opacity-0 hidden' : 'opacity-100'
         }`}
       />
       <Image
         src={previewDark}
         quality={100}
-        width={896}
-        height={558.16}
+        width={650}
+        height={402}
         alt="Dark theme preview"
-        className={`border border-border shadow-2xl dark:shadow-none rounded-2xl transition-opacity duration-300 ${
+        className={`max-h-[402px] border border-border shadow-2xl dark:shadow-none rounded-2xl transition-opacity duration-300 ${
           resolvedTheme === 'light' ? 'opacity-0 hidden' : 'opacity-100'
         }`}
       />
