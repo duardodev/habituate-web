615c09bed675b40a9909f8425ec54c4d0d9f0cb1
Deivit Eduardo <deiviteduardo87@gmail.com>
2025-02-21 16:12:09 -0300

    fix(button): ensure better contrast

--- src/app/(public)/(home)/components/buttons.tsx
+++ src/app/(public)/(home)/components/buttons.tsx
@@ -8,14 +8,14 @@ export function Buttons() {
 
   return (
     <div className="mt-2 flex flex-wrap justify-center items-center gap-4">
-      <Button className="h-9 border-none font-semibold" asChild>
+      <Button className="h-9 border-none" asChild>
         <Link href={userId ? '/management' : '/auth/sign-in'}>Get Started</Link>
       </Button>
 
       <Button
         variant="outline"
         asChild
-        className="h-9 font-semibold bg-secondary-foreground/95 text-background hover:text-background hover:bg-secondary-foreground/85"
+        className="h-9 bg-secondary-foreground/95 text-background hover:text-background hover:bg-secondary-foreground/85"
       >
         <Link href="https://github.com/duardodev/habituate-web" target="_blank">
           <Github className="h-4 w-4 mr-2" />
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+        default: 'bg-green-500 text-white hover:bg-green-500/80',
         destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline: 'border border-input bg-background hover:bg-accent/60 hover:text-accent-foreground',
         secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
