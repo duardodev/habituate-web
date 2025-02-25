ad39ceb2995ae9c932cd9ec59cfda2a611593c8e
Deivit Eduardo <deiviteduardo87@gmail.com>
2025-02-25 11:06:38 -0300

    chore: update robots.txt to disallow specific routes

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: [],
+      disallow: ['/management/', '/auth/sign-in/', '/auth/sign-up/'],
     },
     sitemap: 'https://habituate-duardodev.vercel.app/sitemap.xml',
   };
