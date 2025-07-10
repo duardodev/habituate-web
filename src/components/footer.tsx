4e9ee028b47fa974f2779f7df79e7b7a4df5e1af
Deivit Eduardo <deiviteduardo87@gmail.com>
2025-07-09 23:23:28 -0300

    refactor: update footer text to include current year and author name

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,18 @@
 import Link from 'next/link';
 
 export function Footer() {
+  const year = new Date().getFullYear();
+
   return (
     <footer className="h-[10vh] px-6 py-9 mx-auto mt-auto">
       <p className="text-sm text-center text-foreground/70">
-        © Built by{' '}
-        <Link href="https://duardodev.vercel.app/en" target="_blank" className="underline">
-          duardodev.
+        © {year},{' '}
+        <Link
+          href="https://duardodev.vercel.app/en"
+          target="_blank"
+          className="hover:text-black dark:hover:text-white transition-colors"
+        >
+          Deivit Eduardo.
         </Link>
       </p>
     </footer>
